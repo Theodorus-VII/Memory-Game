3d32dab57d34d2f6e804c442539768946af59a92
Theodorus-VII <tediberhanu777@gmail.com>
2024-02-16 14:23:29 +0300

    final touches

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,63 +1,22 @@
-import { pokemonNames } from "./pokemon_names";
-import { useState, useEffect } from "react";
-import Card from "./Card";
+import { useState } from "react";
 import "../styles/board.css";
+import PlayField from "./PlayField";
+import ScoreBoard from "./ScoreBoard";
+import Modal from "./Modal";
 
 export default function GameBoard() {
-  const [pokemonData, setPokemonData] = useState([]);
   const [scoreBoard, setScoreBoard] = useState({
     history: [],
     bestScore: 0,
     currentScore: 0,
   });
-  const [cardsShowing, setCardsShowing] = useState(true);
+  const [modalOpen, setModalOpen] = useState(true);
 
-  // first needs to fetch the pokemons from the server. will need their names and images then.
-
-  useEffect(() => {
-    // runs onMount
-    async function loadPokemonData() {
-      // fetch the data from the server.
-      const promises = pokemonNames.map(async (pokemon) => {
-        const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`
-        );
-        const result = await response.json();
-        const sprite_url =
-          result["sprites"]["other"]["official-artwork"]["front_default"];
-        return {
-          name: pokemon,
-          id: result["id"],
-          sprite: sprite_url,
-        };
-      });
-      const resolvedData = await Promise.all(promises);
-      setPokemonData(resolvedData);
-    }
-    loadPokemonData();
-  }, []);
-
-  function shuffleArray(array) {
-    // Fisher-Yates Sorting Algorithm.
-    // create a new copy to avoid mutation
-    let copiedArray = [...array];
-    for (let i = copiedArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [copiedArray[i], copiedArray[j]] = [copiedArray[j], copiedArray[i]];
-    }
-    return copiedArray;
-  }
-
-  async function handleClick(id) {
-    // declare variables for the state object.
-    setCardsShowing(false);
-    setTimeout(() => {
-      setCardsShowing(true);
-    }, 300);
-
+  function updateScoreBoard(id) {
     let newCurrentScore;
     let newHistory;
     let newBestScore;
+    let gameState = 0;
 
     if (scoreBoard.history.includes(id)) {
       // game fail state. reset history and current score, update best score.
@@ -66,7 +25,12 @@ export default function GameBoard() {
 
       newBestScore = scoreBoard.bestScore;
       if (scoreBoard.currentScore >= scoreBoard.bestScore) {
+        //new high score
         newBestScore = scoreBoard.currentScore;
+        gameState = 1;
+      } else {
+        //didnt beat high score
+        gameState = -1;
       }
     } else {
       newHistory = [...scoreBoard.history, id];
@@ -82,33 +46,22 @@ export default function GameBoard() {
       currentScore: newCurrentScore,
       bestScore: newBestScore,
     });
-
-    setTimeout(() => {
-      // randomize the pokemonData array for the next go.
-      setPokemonData((prevPokemonData) => {
-        const shuffledData = shuffleArray([...prevPokemonData]);
-        return shuffledData;
-      });
-      // this way triggers a re-render, which stops some of the flip animations(random).
-    }, 1);
-    clearTimeout();
-    // now that we have the new values, update the state object.
+    return gameState;
   }
 
   return (
     <>
-      {/* <div>THE cards go here</div> */}
-      <div className="gameboardContainer">
-        {pokemonData.map((pokemon, index) => (
-          <Card
-            key={pokemon.id}
-            {...pokemon}
-            cardsShowing={cardsShowing}
-            handleClick={handleClick}
-            animationKey={`${index} - ${pokemon.id}`}
-          />
-        ))}
-      </div>
+      {modalOpen ? (
+        <Modal>
+          <h2>Game Over</h2>
+          <button>Try Again?</button>
+        </Modal>
+      ) : (
+        <div className="board-container">
+          <ScoreBoard scoreBoard={scoreBoard} />
+          <PlayField updateScoreBoard={updateScoreBoard} />
+        </div>
+      )}
     </>
   );
 }
--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,11 +1,17 @@
 /* eslint-disable react/prop-types */
 export default function ScoreBoard({ scoreBoard }) {
   return (
-    <div className="score-board">
-      <div>ScoreBoard</div>
-      <div>
-        current score: {scoreBoard.currentScore}
-        best score: {scoreBoard.bestScore}
+    <div className="header">
+      <p className="header-desc">
+        Get points by clicking on images. Game over if you click on any more
+        than once!
+      </p>
+      <div className="score-board">
+        <h1>ScoreBoard</h1>
+        <div className="scores">
+          <div>Current Score: {scoreBoard.currentScore}</div>
+          <div>Best Score: {scoreBoard.bestScore}</div>
+        </div>
       </div>
     </div>
   );
